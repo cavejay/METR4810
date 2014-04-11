c6f51e34c8beb82cade0cbee01792502742cd47b
Michael Ball <i.might.live@gmail.com>
2014-04-11 14:47:48 +1000

    Added Robot Simulator Class and started ignoring output files

---
+++ src/RobotSim.cpp
@@ -0,0 +1,46 @@
+/*
+ * RobotSim.cpp
+ *
+ *  Created on: 11/04/2014
+ *      Author: Michael
+ */
+
+#include "RobotSim.h"
+
+RobotSim::RobotSim(Point2d Position, float angle, String name, Size R_size)
+{
+  this->angle = angle;
+  this->Position = Position;
+  this->R_size = R_size;
+  if(!(name == "")){this->name = name;}
+    else {this->name = "Robot_Name";}
+
+}
+
+void RobotSim::draw(Mat& src)
+{
+  cv::RotatedRect Rekt = RotatedRect(Position, R_size, angle);
+  Point2f vertices[4];
+  Rekt.points(vertices);
+  Point2f Front((vertices[2].x+vertices[3].x)/2, (vertices[2].y+vertices[3].y)/2);
+  cv::line(src, vertices[0], Front, Scalar(255,0,0));
+  cv::line(src, vertices[1], Front, Scalar(255,0,0));
+  cv::line(src, vertices[0], vertices[1], Scalar(255,0,0));
+
+  //  for (int i = 0; i < 4; i++)
+//  {
+//    cv::line(src, vertices[i], vertices[(i+1)%4], Scalar(255,0,0));
+//  }
+}
+
+
+/*
+ * Moves the Robot a certain distance 'dist' in the direction of angle
+ * in respect to it's current anglular direction. *
+ */
+void RobotSim::move(int dist, float angle)
+{
+  Position.x += dist * cos((angle+this->angle)* PI_NUM /180);
+  Position.y += dist * sin((angle+this->angle)* PI_NUM /180);
+  this->angle += angle;
+}
---
+++ src/RobotSim.h
@@ -0,0 +1,45 @@
+/*
+ * RobotSim.h
+ *
+ *  Created on: 11/04/2014
+ *      Author: Michael
+ */
+
+#ifndef ROBOTSIM_H_
+#define ROBOTSIM_H_
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <math.h>
+
+#define PI_NUM 3.14159265
+
+using namespace cv;
+using namespace std;
+
+class RobotSim
+{
+private:
+  // Internal
+  vector<Point2d> History;
+  Size R_size;
+  String name;
+
+public:
+  // Vars
+  cv::Point2d Position;
+  float angle;
+
+  // constructor
+  RobotSim (Point2d Position, float angle, String name = "", Size R_size = Size(60,35));
+
+  // Functions
+  void draw(Mat& src);
+  void move(int dist, float angle);
+};
+
+#endif /* ROBOTSIM_H_ */
